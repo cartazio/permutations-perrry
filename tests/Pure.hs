b37c608899106ed1dd02446ae5c4f8fb3b375211
Patrick Perry <patperry@stanford.edu>
2008-12-05 02:56:01 +0000

    Add prop_apply test

--- tests/Pure.hs
+++ tests/Pure.hs
@@ -16,7 +16,12 @@ prop_elems_permute =
     forAll arbitrary $ \(Nat n) ->
         elems (permute n) == [0..(n-1)]
 
+prop_apply =
+    forAll arbitrary $ \(Index i n) ->
+    forAll (Test.permute n) $ \p ->
+        apply p i == (elems p) !! i
 
 tests_Permute = 
     [ ("elems . permute", mytest prop_elems_permute)
+    , ("apply"          , mytest prop_apply)
     ]
