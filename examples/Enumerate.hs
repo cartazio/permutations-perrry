3acfc7cc50d40ec6761fd9924e01ee70da93cc71
Patrick Perry <patperry@stanford.edu>
2008-12-05 23:32:13 +0000

    Add Enumerate.hs example

---
+++ examples/Enumerate.hs
@@ -0,0 +1,54 @@
+module Main where
+    
+import Control.Monad
+import Control.Monad.ST
+import Data.STRef
+import System.Environment
+
+import Data.Permute
+import Data.Permute.MPermute
+    
+-- | Execute an action on every permutation of a given order.
+forAllPermutes :: (MPermute p m) => Int -> (Permute -> m ()) -> m ()
+forAllPermutes n a = do
+    -- Allocate a mutable permutation initialized to the identity
+    p  <- newPermute n
+    
+    -- Run the action on all successors of p
+    runOnSuccessors p
+    
+  where
+    runOnSuccessors p = do
+        -- Cast the mutable permutation to an immutable one
+        p' <- unsafeFreeze p  
+
+        -- Run the action on the immutable permutation
+        a p'
+
+        -- Set the permutation to be equal to its successor
+        hasNext <- setNext p
+        
+        -- If a successor exists, recurse, otherwise stop
+        when hasNext $ runOnSuccessors p
+
+        
+-- | Count the number of permutations of a given order
+countAllPermutes :: Int -> Int
+countAllPermutes n = runST $ do
+    count <- newSTRef 0
+    forAllPermutes n (const $ modifySTRef count (+1))
+    readSTRef count
+
+        
+-- | Print all permutations of a given order.
+printAllPermutes :: Int -> IO ()
+printAllPermutes n =
+    forAllPermutes n (putStrLn . show . elems)
+    
+
+main = do
+    n  <- fmap (read . head)getArgs
+    let count = countAllPermutes n
+    putStrLn $ 
+        "There are " ++ show count ++ " permutations of order " ++ show n ++ "."
+    
